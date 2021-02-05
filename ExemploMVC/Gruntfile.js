22d36180b58f270f2bc9358525f74692a6a00233
Cleiton Alves <cleiton.alvesjc@gmail.com>
2021-02-05 10:08:00 -0300

    add grunt in project for teste

---
+++ ExemploMVC/Gruntfile.js
@@ -0,0 +1,21 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        minifyHtml:
+        {
+            Html: {
+                src: ['wwwroot/Htmlfiles/**/*.html'],
+                dest: 'wwwroot/Compressedfiles/',
+                expand: true,
+                flatten: true
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-minify-html");
+
+    // A very basic default task.
+    grunt.registerTask('default', 'Task Description', function () {
+        grunt.log.write('Welcome to Grunt js in Visual Studio');
+    });
+
+};
+
